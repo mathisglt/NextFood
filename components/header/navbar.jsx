9db7aec73a33cbfb25b531b1f7d5768572203699
Mathis <181504723+mathisglt@users.noreply.github.com>
2024-12-05 16:25:49 +0100

    pre database 107

--- components/header/navbar.jsx
+++ components/header/navbar.jsx
@@ -1,8 +1,11 @@
+"use client"
 import Link from "next/link"
 import Image from "next/image"
 import logoImg from "assets/logo.png"
 import classes from "./navbar.module.css"
+import { usePathname } from "next/navigation"
 export default function Navbar() {
+    const path = usePathname();
     return (
         <header className={classes.header}>
             <Link className={classes.logo} href="/">
@@ -12,10 +15,10 @@ export default function Navbar() {
             <nav className={classes.nav}>
                 <ul className={classes.ul}>
                     <li className={classes.li}>
-                        <Link href="/meals">Repas</Link>
+                        <Link href="/meals" className={path.startsWith('/meals') ? classes.active:undefined}>Repas</Link>
                     </li>
                     <li>
-                        <Link href="/community">Communauté</Link>
+                        <Link href="/community" className={path.startsWith('/community') ? classes.active:undefined}>Communauté</Link>
                     </li>
                 </ul>
             </nav>
