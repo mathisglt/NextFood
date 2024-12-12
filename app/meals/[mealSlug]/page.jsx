2e8bf6fc59c62ccb00bffd2d4dd2dba576261613
Mathis <181504723+mathisglt@users.noreply.github.com>
2024-12-13 00:16:02 +0100

    fix : MealDetail page style update

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -29,20 +29,18 @@ export default async function MealsSlugDetail({ params }) {
         <>
             <header className={classes.header}>
                 <div className={classes.image}>
-                    <Image src={meal.image} alt={meal.title} fill />
+                    <Image src={meal.image} alt={meal.title} fill priority />
                 </div>
                 <div className={classes.headerText}>
                     <h1>{meal.title}</h1>
                     <p className={classes.creator}>
-                        <a href={`mailto:${meal.creator_email}`}>{meal.creaton}</a>
+                        par <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
                     </p>
-                    <p className={classes.summary}>
-                        <a href={`mailto:${meal.summary}`}>Résumé</a>
-                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
                 </div>
-
             </header>
-            <main>
+            <main className={classes.main}>
+                <h2>Instructions</h2>
                 <p
                     className={classes.instructions}
                     dangerouslySetInnerHTML={{
